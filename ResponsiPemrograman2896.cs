23492073163c9a93f2dcdfd21a4c144786aeb5b0
reny <Reny274apr>
2020-04-15 15:23:53 +0700

    Responsi Pemrograman

---
+++ ResponsiPemrograman2896.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ResponsiPemrograman2893
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Karyawan karyawan1 = new Karyawan(19112896, "Reny", 800000);
+            Karyawan karyawan2 = new Karyawan(19020117, "Hapiz", 700000);
+
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan1.showKaryawan();
+            karyawan2.showKaryawan();
+
+            Console.WriteLine("\n\n hey baby naik gaji nih 10%");
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan1.naikGaji();
+            karyawan2.naikGaji();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Karyawan
+    {
+        public int nik { get; set; }
+        public string nama { get; set; }
+        public int gajiBulanan { get; set; }
+
+        public Karyawan(int nik, string nama, int gaji)
+        {
+            this.nik = nik;
+            this.nama = nama;
+
+            if (gaji < 0)
+            {
+                this.gajiBulanan = 0;
+            }
+            else
+            {
+                this.gajiBulanan = gaji;
+            }
+        }
+
+        public void showKaryawan()
+        {
+            Console.WriteLine("{0} \t {1} \t {2}", nik, nama, gajiBulanan);
+        }
+
+        public void naikGaji()
+        {
+            double tmp = 0;
+            tmp = 0.1 * gajiBulanan;
+            gajiBulanan += Convert.ToInt32(tmp);
+            Console.WriteLine("{0} \t {1} \t {2}", nik, nama, gajiBulanan);
+        }
+    }
+}
